d3be2cacd49623386781f2eb282a6946a726aaa3
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2020-01-23 21:23:42 -0500

    /people now returns all of the people available at swapi/people (page 1 to 9) in a single endpoint

--- server/routes/person.js
+++ server/routes/person.js
@@ -5,9 +5,6 @@ const swapi = require("swapi-node");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  swapi.getPerson(1).then((result) => {
-    console.log(typeof result);
-})
   res.send('respond with a resource');
 });
 
