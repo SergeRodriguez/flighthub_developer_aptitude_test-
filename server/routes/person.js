1846a8cc54256baa4d5443b5b6d57ed94dad567a
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2020-01-23 20:07:57 -0500

    install nodemon and add script

--- server/routes/person.js
+++ server/routes/person.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const swapi = require("swapi-node");
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  swapi.getPerson(1).then((result) => {
+    console.log(typeof result);
+})
   res.send('respond with a resource');
 });
 
