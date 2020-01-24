b5f6f6ac9a0d4df3658e88206cbe55bbf6938b5c
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2020-01-23 21:50:13 -0500

    /planets now returns all of the planets available at swapi/planets  (page 1 to 7) in a single endpoint

--- server/routes/planets.js
+++ server/routes/planets.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const swapi = require("swapi-node");
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  Promise.all([
+     
+    Promise.resolve(swapi.get('https://swapi.co/api/planets/?page=1')),
+    Promise.resolve(swapi.get('https://swapi.co/api/planets/?page=2')),
+    Promise.resolve(swapi.get('https://swapi.co/api/planets/?page=3')),
+    Promise.resolve(swapi.get('https://swapi.co/api/planets/?page=4')),
+    Promise.resolve(swapi.get('https://swapi.co/api/planets/?page=5')),
+    Promise.resolve(swapi.get('https://swapi.co/api/planets/?page=6')),
+    Promise.resolve(swapi.get('https://swapi.co/api/planets/?page=7')),
+
+  ]).then((all) => {
+    res.send([...all[0].results, ...all[1].results, ...all[2].results, ...all[3].results,
+    ...all[4].results, ...all[5].results, ...all[6].results])
+  })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
